69f9279aecafcb335cddba69ed5a73910999f0f8
Pete Cordell <git@codalogic.com>
2012-05-25 08:01:20 +0100

    Refactored reader tests to to use DI for the type of reader to use for the tests

--- Test/reader-test.cpp
+++ Test/reader-test.cpp
@@ -37,138 +37,179 @@
 
 using namespace cl;
 
-TFUNCTION( string_reader_basic_test )
+class reader_factory
 {
-	TBEGIN( "String reader basic tests" );
-	
-	TSETUP( reader_string my_reader( "abcd" ) );
-	
-	TTEST( my_reader.get() == 'a' );
-	TTEST( my_reader.current() == 'a' );
-	TTEST( my_reader.current() == 'a' );	// Can read current() multiple times
-	TTEST( my_reader.get() == 'b' );
-	TTEST( my_reader.current() == 'b' );
+public:
+	virtual reader * create( const char * p_data ) const = 0;
+	virtual const char * form_name() const = 0;
+};
 
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.get() == 'd' );
+class reader_factory_string : public reader_factory
+{
+public:
+	virtual reader * create( const char * p_data ) const
+	{
+		return new reader_string( p_data );
+	}
+	virtual const char * form_name() const
+	{
+		return "String";
+	}
+};
+
+class reader_factory_file : public reader_factory
+{
+public:
+	virtual reader * create( const char * p_data ) const
+	{
+		TTODO( "create reader_factory_file::create();" );
+	}
+	virtual const char * form_name() const
+	{
+		return "File";
+	}
+};
+
+void reader_basic_test( reader_factory & r_reader_factory )
+{
+	TBEGIN( (std::string( r_reader_factory.form_name() ) + " reader basic tests").c_str() );
 	
-	TTEST( my_reader.get() == reader::R_EOI );
-	TTEST( my_reader.current() == reader::R_EOI );
+	TSETUP( std::auto_ptr< reader > p_reader( r_reader_factory.create( "abcd" ) ) );
 	
-	TSETUP( reader_string my_reader_empty( "" ) );
+	TTEST( p_reader->get() == 'a' );
+	TTEST( p_reader->current() == 'a' );
+	TTEST( p_reader->current() == 'a' );	// Can read current() multiple times
+	TTEST( p_reader->get() == 'b' );
+	TTEST( p_reader->current() == 'b' );
+
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->get() == 'd' );
 	
-	TTEST( my_reader_empty.get() == reader::R_EOI );
-	TTEST( my_reader_empty.current() == reader::R_EOI );
+	TTEST( p_reader->get() == reader::R_EOI );
+	TTEST( p_reader->current() == reader::R_EOI );
+	
+	TSETUP( std::auto_ptr< reader > p_reader_empty( r_reader_factory.create( "" ) ) );
+	
+	TTEST( p_reader_empty->get() == reader::R_EOI );
+	TTEST( p_reader_empty->current() == reader::R_EOI );
 }
 
-TFUNCTION( string_reader_location_test )
+void reader_location_test( reader_factory & r_reader_factory )
 {
-	TBEGIN( "String reader location tests" );
+	TBEGIN( (std::string( r_reader_factory.form_name() ) + " reader location tests").c_str() );
 	
-	TSETUP( reader_string my_reader( "abcdef" ) );
+	TSETUP( std::auto_ptr< reader > p_reader( r_reader_factory.create( "abcdef" ) ) );
 	
-	TTEST( my_reader.get() == 'a' );
-	TTEST( my_reader.current() == 'a' );
-	TTEST( my_reader.current() == 'a' );	// Can read current() multiple times
-	TTEST( my_reader.get() == 'b' );
-	TTEST( my_reader.current() == 'b' );
+	TTEST( p_reader->get() == 'a' );
+	TTEST( p_reader->current() == 'a' );
+	TTEST( p_reader->current() == 'a' );	// Can read current() multiple times
+	TTEST( p_reader->get() == 'b' );
+	TTEST( p_reader->current() == 'b' );
 
-	TTEST( my_reader.location_push() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->location_push() );	// Not really a test, but we want to ensure it returns true
 	
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.get() == 'd' );
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->get() == 'd' );
 
-	TTEST( my_reader.location_push() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->location_push() );	// Not really a test, but we want to ensure it returns true
 
-	TTEST( my_reader.get() == 'e' );
-	TTEST( my_reader.get() == 'f' );
-	TTEST( my_reader.get() == reader::R_EOI );
-	TTEST( my_reader.get() == reader::R_EOI );	// Should be able to repeatedly call get() once read end of input
+	TTEST( p_reader->get() == 'e' );
+	TTEST( p_reader->get() == 'f' );
+	TTEST( p_reader->get() == reader::R_EOI );
+	TTEST( p_reader->get() == reader::R_EOI );	// Should be able to repeatedly call get() once read end of input
 	
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'e' );
-	TTEST( my_reader.get() == 'f' );
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'e' );
-	TTEST( my_reader.get() == 'f' );
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'e' );
+	TTEST( p_reader->get() == 'f' );
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'e' );
+	TTEST( p_reader->get() == 'f' );
 	
-	TTEST( my_reader.location_pop() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.get() == 'd' );
+	TTEST( p_reader->location_pop() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->get() == 'd' );
 }
 
-TFUNCTION( string_reader_unget_test )
+void reader_unget_test( reader_factory & r_reader_factory )
 {
-	TBEGIN( "String reader unget/peek tests" );
+	TBEGIN( (std::string( r_reader_factory.form_name() ) + " reader unget/peek tests").c_str() );
 	
-	TSETUP( reader_string my_reader( "abcdef" ) );
+	TSETUP( std::auto_ptr< reader > p_reader( r_reader_factory.create( "abcdef" ) ) );
 	
-	TTEST( my_reader.get() == 'a' );
-	TTEST( my_reader.get() == 'b' );
-	TSETUP( my_reader.unget() );
-	TTEST( my_reader.current() == 'b' );	// current() char not affected by unget() (i.e. doesn't make current() return 'a' here)
-	TTEST( my_reader.get() == 'b' );
-	TSETUP( my_reader.unget() );
-	TTEST( my_reader.current() == 'b' );	// current() char not affected by unget()
-	TTEST( my_reader.get() == 'b' );
-	TSETUP( my_reader.unget( 'z' ) );
-	TSETUP( my_reader.unget( 'y' ) );
-	TTEST( my_reader.current() == 'b' );	// current() char not affected by unget()
-	TTEST( my_reader.get() == 'y' );		// ungetted chars returned in reverse order to what they were unget()ted
-	TTEST( my_reader.get() == 'z' );
+	TTEST( p_reader->get() == 'a' );
+	TTEST( p_reader->get() == 'b' );
+	TSETUP( p_reader->unget() );
+	TTEST( p_reader->current() == 'b' );	// current() char not affected by unget() (i.e. doesn't make current() return 'a' here)
+	TTEST( p_reader->get() == 'b' );
+	TSETUP( p_reader->unget() );
+	TTEST( p_reader->current() == 'b' );	// current() char not affected by unget()
+	TTEST( p_reader->get() == 'b' );
+	TSETUP( p_reader->unget( 'z' ) );
+	TSETUP( p_reader->unget( 'y' ) );
+	TTEST( p_reader->current() == 'b' );	// current() char not affected by unget()
+	TTEST( p_reader->get() == 'y' );		// ungetted chars returned in reverse order to what they were unget()ted
+	TTEST( p_reader->get() == 'z' );
 	
 	TDOC( "Test peek()" );
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.peek() == 'd' );
-	TTEST( my_reader.peek() == 'd' );
-	TTEST( my_reader.get() == 'd' );
-	TTEST( my_reader.peek() == 'e' );
-	TTEST( my_reader.get() == 'e' );
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->peek() == 'd' );
+	TTEST( p_reader->peek() == 'd' );
+	TTEST( p_reader->get() == 'd' );
+	TTEST( p_reader->peek() == 'e' );
+	TTEST( p_reader->get() == 'e' );
 }
 
-TFUNCTION( string_reader_location_logger_test )
+void string_reader_location_logger_test( reader_factory & r_reader_factory )
 {
-	TBEGIN( "String reader location logger tests" );
-	
-	
-	TSETUP( reader_string my_reader( "abcdef" ) );
-	
-	TTEST( my_reader.get() == 'a' );
-	TTEST( my_reader.current() == 'a' );
-	TTEST( my_reader.current() == 'a' );	// Can read current() multiple times
-	TTEST( my_reader.get() == 'b' );
-	TTEST( my_reader.current() == 'b' );
+	TBEGIN( (std::string( r_reader_factory.form_name() ) + " reader location logger tests").c_str() );
 
-	TTEST( my_reader.location_push() );	// Not really a test, but we want to ensure it returns true
+	TSETUP( std::auto_ptr< reader > p_reader( r_reader_factory.create( "abcdef" ) ) );
 	
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.get() == 'd' );
+	TTEST( p_reader->get() == 'a' );
+	TTEST( p_reader->current() == 'a' );
+	TTEST( p_reader->current() == 'a' );	// Can read current() multiple times
+	TTEST( p_reader->get() == 'b' );
+	TTEST( p_reader->current() == 'b' );
+
+	TTEST( p_reader->location_push() );	// Not really a test, but we want to ensure it returns true
+	
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->get() == 'd' );
 	
 	{	// Control scope of my_location_logger
-	TSETUP( location_logger my_location_logger( my_reader ) );
+	TSETUP( location_logger my_location_logger( *p_reader ) );
 
-	TTEST( my_reader.get() == 'e' );
-	TTEST( my_reader.get() == 'f' );
-	TTEST( my_reader.get() == reader::R_EOI );
-	TTEST( my_reader.get() == reader::R_EOI );	// Should be able to repeatedly call get() once read end of input
+	TTEST( p_reader->get() == 'e' );
+	TTEST( p_reader->get() == 'f' );
+	TTEST( p_reader->get() == reader::R_EOI );
+	TTEST( p_reader->get() == reader::R_EOI );	// Should be able to repeatedly call get() once read end of input
 	
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'e' );
-	TTEST( my_reader.get() == 'f' );
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'e' );
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'e' );
+	TTEST( p_reader->get() == 'f' );
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'e' );
 	}	// End of my_location_logger scope
 	
-	TTEST( my_reader.get() == 'f' );
-	TTEST( my_reader.location_top() );	// Not really a test, but we want to ensure it returns true
-	TTEST( my_reader.get() == 'c' );
-	TTEST( my_reader.get() == 'd' );
+	TTEST( p_reader->get() == 'f' );
+	TTEST( p_reader->location_top() );	// Not really a test, but we want to ensure it returns true
+	TTEST( p_reader->get() == 'c' );
+	TTEST( p_reader->get() == 'd' );
+}
+
+void all_reader_tests( reader_factory & r_reader_factory )
+{
+	reader_basic_test( r_reader_factory );
+}
+
+TFUNCTION( string_reader_test )
+{
+	TBEGIN( "String reader tests" );
+	
 }
 
 TFUNCTION( file_reader_test )
 {
-	TBEGIN( "File reader tests" );
-	
 	TTODO( "File reader tests" );
 }
